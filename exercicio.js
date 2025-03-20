887765134a630f5e32373a612df91a83cedca338
FariaMarcus <marcusfaria888@gmail.com>
2025-03-19 23:51:34 -0300

    Add files via upload

---
+++ exercicio.js
@@ -0,0 +1,16 @@
+function setRowRed(id_Row) {
+    document.getElementById(id_Row).style.background = 'red'; 
+}
+function setRowGreen(linha_colorida) {
+    document.getElementById(linha_colorida).style.background ='green';
+}
+
+function onMouseOver(pintaLinha) {
+    document.getElementById(pintaLinha);
+}
+function getCellValue(id_cel){
+    alert(document.getElementById(id_cel).innerHTML);
+    console.log(document.getElementById(id_cel).innerHTML);
+}
+
+
